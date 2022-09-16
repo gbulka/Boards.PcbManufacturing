5f7edfe6a9150e7c063b4e4b31df13b54e0f0142
graegh <graegh@mailmix.pl>
2022-09-16 22:56:34 +0200

    Updated views.

--- Boards.PcbManufacturing/ViewModels/PreferencesViewModel.cs
+++ Boards.PcbManufacturing/ViewModels/PreferencesViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Boards.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using Boards.Dto;
 using Boards.PcbManufacturing.BaseModel;
 
 namespace Boards.PcbManufacturing.ViewModels
@@ -13,6 +15,8 @@ namespace Boards.PcbManufacturing.ViewModels
         private SurfaceFinish _surfaceFinish;
         private Color _color;
 
+        [Required(ErrorMessage = "Project Name is required.")]
+        [StringLength(50, ErrorMessage = "No more than 50 characters")]
         public string ProjectName
         { 
             get => _projectName; 
@@ -23,6 +27,12 @@ namespace Boards.PcbManufacturing.ViewModels
             }
         }
 
+        [Required(ErrorMessage = "Zipcode is required.")]
+        [RegularExpression(
+            @"(^\d{5}(-\d{4})?$)"+
+            @"|(^[ABCEGHJKLMNPRSTVXYabceghjklmnprstvxy]{1}\d{1}[ABCEGHJKLMNPRSTVWXYZabceghjklmnprstv‌​xy]{1} *\d{1}[ABCEGHJKLMNPRSTVWXYZabceghjklmnprstvxy]{1}\d{1}$)"+
+            @"|\d{2}-\d{3}", 
+            ErrorMessage = "That postal code is not a valid US or Canadian postal code.")]
         public string ZipCode
         { 
             get => _zipCode; 
