564693ac97c87c5ee637bfbfb2875b3c64458994
graegh <graegh@mailmix.pl>
2022-09-18 22:28:13 +0200

    Defined a dependency property to resolve the column captions from attributes of the row view model.

--- Boards.PcbManufacturing/ViewModels/QuoteEntryViewModel.cs
+++ Boards.PcbManufacturing/ViewModels/QuoteEntryViewModel.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Boards.PcbManufacturing.ViewModels
 {
     public class QuoteEntryViewModel
     {
         public string Group { get; set; }
+
+        [Display(Name = "Parameter Name")]
         public string ParameterName { get; set; }
+
+        [Display(Name = "Parameter Value")]
         public string ParameterValue { get; set; }
+
+        [Display(Name = "Time Impact")]
         public TimeSpan TimeImpact { get; set; }
+
+        [Display(Name = "Cost Impact")]
         public decimal CostImpact { get; set; }
     }
 }
